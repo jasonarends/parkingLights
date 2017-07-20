0f567b50586b4196ebc8eb134244cff268d08a64
Jason Arends <jason.arends@gmail.com>
2017-07-20 13:51:30 -0500

    moved light patterns out, more patterns

--- parkingLights.py
+++ parkingLights.py
@@ -18,39 +18,71 @@ def gpioSetup():
     GPIO.set_mode(GPIO_ECHO, pigpio.INPUT)
 
 def colors():
-    global distance, speed
-    speed = 0.05
+    global distance
     distance = 0
+    
     while True:
-        for i in range(7,0,-1):
-            #print distance
-            if distance > 600: # max response time
-                #print 'door open'
-                h = 0.6
-                speed = 0.5
-            elif 300 < distance < 600: # entering garage - green
-                #print 'green'
-                h = 0.33
-                speed = 0.1
-            elif 140 < distance < 300: # change to yellow at 250
-                #print 'yellow'
-                h = 0.17
-                speed = 0.2
-            elif distance < 140: # change to red at 100
-                #print 'red'
-                h = 0.0
-                speed = 0.5
-            else: # unknown
-                #print 'fail'
-                h = 0.85
-            clear()
-            y = 1.0
-            for x in range(0,5):
-                r,g,b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, 1, y)]
-                set_pixel((i+x)%8, r, g, b)
-                y = y *0.55
+        while distance > 800: #testing with max response 800 since i know it usually shows >2000 when empty
+            for i in range(7,0,-1):
+                clear()
+                y = 1.0
+                for x in range(0,3):
+                    r,g,b = [int(c * 255) for c in colorsys.hsv_to_rgb(0.6, 1, y)] #slow purple 3 dots
+                    set_pixel((i+x)%8, r, g, b)
+                    y = y *0.5
+                show()
+                time.sleep(0.4)
+                
+        while 400 < distance < 800: #entering garage - green fast 4 dots
+            for i in range(7,0,-1):
+                clear()
+                y = 1.0
+                for x in range(0,4):
+                    r,g,b = [int(c * 255) for c in colorsys.hsv_to_rgb(0.33, 1, y)]
+                    set_pixel((i+x)%8, r, g, b)
+                    y = y *0.5
+                show()
+                time.sleep(0.1)
+                
+        while 140 < distance < 400: #entering garage - yellow slower 5 dots
+            for i in range(7,0,-1):
+                clear()
+                y = 1.0
+                for x in range(0,5):
+                    r,g,b = [int(c * 255) for c in colorsys.hsv_to_rgb(0.16, 1, y)]
+                    set_pixel((i+x)%8, r, g, b)
+                    y = y *0.55
+                show()
+                time.sleep(0.2)
+
+        while 120 < distance < 140: #entering garage - within range, red slow 6 dots
+            for i in range(14,0,-1):
+                j = int(i/2)
+                clear()
+                y = 1.0
+                for x in range(0,6):
+                    r,g,b = [int(c * 255) for c in colorsys.hsv_to_rgb(0.0, 1, y)]
+                    set_pixel((j+x)%8, r, g, b)
+                    y = y *0.5
+                show()
+                time.sleep(0.25) #updates faster than .5 and changed range to 0-14 but divides it by 2 so only moves half as fast as it updates
+        
+        while 105 < distance < 120: #entering garage - red, stopped
+            for x in range(0,7):
+                set_pixel(x, 255, 0, 0)
             show()
-            time.sleep(speed)
+            time.sleep(0.1)
+            
+        while distance < 105: #entering garage - green fast 4 dots
+            for i in range(7,0,1):
+                clear()
+                y = 1.0
+                for x in range(0,6):
+                    r,g,b = [int(c * 255) for c in colorsys.hsv_to_rgb(0.33, 1, y)]
+                    set_pixel((i-x)%8, r, g, b)
+                    y = y *0.5
+                show()
+                time.sleep(0.1)
 
 def cbfStart(gpio, level, tick):
     global startTick
